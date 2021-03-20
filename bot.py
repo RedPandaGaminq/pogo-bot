2825c43307589ee66cef56f1fc2a282dd211c267
euouae682 <jasonuzhao682@gmail.com>
2021-03-20 15:33:03 -0700

    Added spaces between the command code for readability.

--- bot.py
+++ bot.py
@@ -24,6 +24,7 @@ async def on_message(message):
     # Commands
     if '.nuke' == message.content.lower():
         await message.channel.send('This command has been temporarily disabled.')
+
     if message.content.lower().startswith(".join"):
         output = message.content.split()
         if "@everyone" in output:
@@ -33,6 +34,7 @@ async def on_message(message):
                 await message.channel.send("{}, JUAN NOW DUDE".format(output[1]))
         else:
             await message.channel.send("Error: Invalid Input")
+
     if '.penis' in message.content.lower():
         await message.channel.send("{}'S PENIS SIZE: 8{}>".format(message.content[7:], random.randint(0, 20) * "="))
 
