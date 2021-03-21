65eafae5e835f96eff847702402078b1521f2c45
Timmie <timothyzhong2@gmail.com>
2021-03-20 19:48:25 -0700

    Updated Nuke

--- bot.py
+++ bot.py
@@ -23,7 +23,30 @@ async def on_message(message):
 
     # Commands
     if '.nuke' == message.content.lower():
-        await message.channel.send('This command has been temporarily disabled.')
+        number = random.randint(0, 9999)
+        await message.channel.send('Warning! Prototype Nuke {} has a 99% fail rate! Failure will be devastating!'
+                                   .format(number))
+        await message.channel.send("Are you sure you want to launch Nuke {}?".format(number))
+
+        def check(m):
+            return m.content.lower() == 'yes' or m.content.lower == 'no'
+
+        message = await client.wait_for('message', check=check)
+        if message.content.lower() == 'yes':
+            for i in range(10):
+                await message.channel.send("WARNING! LAUNCHING NUKE {}!".format(number))
+            for i in range(10, 0, -1):
+                await message.channel.send(i)
+            if random.randint(1, 100) == 1:
+                await message.channel.send("NUKE LAUNCH SUCCESSFUL!")
+                for i in range(20):
+                    await message.channel.send("@everyone")
+            else:
+                await message.channel.send("Launch failed... you will now be punished for wasting so much money...")
+                for i in range(20):
+                    await message.author.send("YOU HAVE BEEN NUKED!")
+        else:
+            await message.channel.send("Lame... goodbye.")
 
     if message.content.lower().startswith(".join"):
         output = message.content.split()
@@ -38,16 +61,16 @@ async def on_message(message):
     if message.content.lower().startswith(".penis"):
         output = message.content.split()
         if "@everyone" in output:
-            await message.author.send("YOUR PP SMALL LOL")
+            await message.author.send("YOUR PP SMALL OMEGALUL")
         elif len(output) == 2:
             await message.channel.send("{}'S PENIS SIZE: 8{}>".format(message.content[7:], random.randint(0, 20) * "="))
         else:
             await message.channel.send("Error: Invalid Input")
 
     if '.ppbegin' == message.content.lower():
-        ppnames = ["determined dong", "dangerous dick", "colossal cock", "pleasant penis"]
-        ppname = ppnames[random.randint(0, 3)]
-        await message.channel.send("The story of your {} begins...".format(ppname))
+        pp_names = ["determined dong", "dangerous dick", "colossal cock", "pleasant penis"]
+        pp_name = pp_names[random.randint(0, 3)]
+        await message.channel.send("The story of your {} begins...".format(pp_name))
 
     # Replies
     if 'horny' in message.content.lower():
